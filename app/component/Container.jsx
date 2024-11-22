0b0085699a0353f4c5092b453851a63b9745d7e4
akash100a <arakash968786@gmail.com>
2024-11-23 00:37:13 +0600

    up

--- app/component/Container.jsx
+++ app/component/Container.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Container = () => {
+const Container = ({children,className}) => {
   return (
-    <div>Container</div>
+    <div className={`${className} max-w-[1400px] mx-auto`}>
+        {children}
+    </div>
   )
 }
 
